8f095275b3ac0f86f456a680f40a2c0b7f104144
dilip <dilip.roy4892@gmail.com>
2025-02-18 21:20:10 +0530

    taskamanger added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Nav } from "./components/navbar/Appbar.js";
+import { Container } from "@mui/material";
+import { TaskManager } from "./components/TaskManager/TaskManager.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Container maxWidth="lg">
+        <TaskManager />
+      </Container>
+    </>
   );
 }
 
